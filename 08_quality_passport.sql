3173eb5ea41d5c3841d2fcb30d6bb01dfff4b39e
Narchitect07 <narchmanagement@gmail.com>
2025-12-30 20:12:32 +0700

    Main

--- 08_quality_passport.sql
+++
@@ -1,6 +0,0 @@
-
--- 08_quality_passport.sql
-
--- Add the quality_passport column to the products table
-ALTER TABLE products
-ADD COLUMN quality_passport JSONB;
--- src/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-
-export const formatPrice = (price: number) => `Rp ${Math.round(price).toLocaleString("id-ID")}`
